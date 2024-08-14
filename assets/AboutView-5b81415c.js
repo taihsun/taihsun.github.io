facb6893af1514f5a90e6fb91880c7708b96ea00
taihsun <taihsun@gmail.com>
2024-08-14 14:01:36 +0800

    clear

--- assets/AboutView-5b81415c.js
+++
@@ -1,1 +0,0 @@
-import{d as e,o,f as t,C as a,F as n,g as r}from"./index-a622a1bc.js";const s=r("br",null,null,-1),u=e({__name:"AboutView",setup(c){return(l,_)=>(o(),t(n,null,[a(" Program Version: dv002"),s],64))}});export{u as default};
