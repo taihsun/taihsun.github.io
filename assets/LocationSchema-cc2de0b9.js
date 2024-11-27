06e859ebeef52837bdefcfee9c56f82bcee7113c
taihsun <taihsun@gmail.com>
2024-11-27 14:01:06 +0800

    demo 31

--- assets/LocationSchema-cc2de0b9.js
+++
@@ -1,1 +0,0 @@
-import{ap as a,aq as e,b7 as o}from"./index-69085551.js";const t=a().shape({name:e}),c=a().shape({name:e,status:o});export{t as c,c as m};
---
+++ assets/LocationSchema-f76179a8.js
@@ -0,0 +1,1 @@
+import{aq as a,ar as e,aT as o}from"./index-bda4bfce.js";const t=a().shape({name:e}),c=a().shape({name:e,status:o});export{t as c,c as m};
